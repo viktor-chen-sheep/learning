be4c27dcd5ae2599c11b7897cfabca6092f9ea85
viktor-chen-sheep <79353418+viktor-chen-sheep@users.noreply.github.com>
2021-02-20 01:40:56 -0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+#def helloN(name) :
+    #print("hello world!", name)
+
+
+#调用函数
+#helloN('neo')
+
+def add(a,b) :
+    return a+b
+
+def reduce(a,b) :
+    return a-b
+
+def multiply(a,b) :
+    return a*b
+
+def divide(a,b) :
+    return a/b
+
+print(add(1,2))
+print(reduce(12,2))
+print(multiply(6,3))
+print(divide(12,6))
+'''
+输出和
+输出差
+输出乘积
+输出商
+'''
---
+++ 序列.py
@@ -0,0 +1,6 @@
+>>> stuinfo=['zhangsan','lisi','wangwu',18,20]
+>>> stuname=['zhangsan','lisi','wangwu']
+>>> stuage=[18,20,16]
+>>> database=[stuname,stuage]
+>>> database
+[['zhangsan','lisi','wangwu'],[18,20,16]]
